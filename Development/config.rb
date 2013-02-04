d1c69968e5f345e1ba3c66fe7c7f6ca7f0d5d292
Suleiman Leadbitter <suleiman.leadbitter@gmail.com>
2013-02-04 15:14:02 +0000

    Initial Commit of project start up files

---
+++ Development/config.rb
@@ -0,0 +1,31 @@
+# Require any additional compass plugins here.
+
+# require 'normalize';
+require 'susy';
+require 'sassy-buttons';
+require 'sassy_noise';
+require 'harsh';
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "build/css"
+sass_dir = "project/assets/sass"
+images_dir = "build/img"
+javascripts_dir = "build/js"
+fonts_dir = "build/fonts"
+
+output_style = :nested
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass project/assets/sass scss && rm -rf sass && mv scss sass
